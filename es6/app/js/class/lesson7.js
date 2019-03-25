ffc46c2918ab88060d56f0ab06c311bdfe22576d
guo <503392549@qq.com>
2019-03-25 13:40:47 +0800

    箭头函数

--- es6/app/js/class/lesson7.js
+++ es6/app/js/class/lesson7.js
@@ -3,16 +3,16 @@
     function news(x,y="worde"){ 
         console.log(x,y);
     }
-    news("hello"); //hello worde
-    news("hello","kill");
+    //news("hello"); //hello worde
+    //news("hello","kill");
 }
 {
     let x = "test";
     function test2(x,y=x){
         console.log(x,y);
     }
-    test2('kill!'); // kill kill
-    console.log(x); //text
+    //test2('kill!'); // kill kill
+    //console.log(x); //text
 }
 {
 
@@ -20,7 +20,7 @@
     function test2(c,y=x){  //y='test'
         console.log(c,y);
     }
-    test2('kill!'); // kill kill
+    //test2('kill!'); // kill kill
     
 }
 {
@@ -29,8 +29,28 @@
             console.log(v);
         }
     }
-    test3(1,2,3,4,'a');
+    //test3(1,2,3,4,'a');
 }
 {
-    console.log(...[1,2,3]);//输出单个元素，而不是数组格式
+    //console.log(...[1,2,3]);//输出单个元素，而不是数组格式
+}
+
+{
+    //this绑定，箭头函数
+    let arrow= v=>v+2;  //arrow是函数 ，v是参数，v+2是返回值
+    let arrow2 = ()=>5; //
+    console.log(arrow(3));
+    console.log(arrow2());
+
+}
+{
+    //尾调用
+    //函数的最后一句话 是否还是函数 ,提升性能
+    function tail(x){
+        console.log('tail',x);
+    }
+    function fx(x){
+        return tail(x);
+    }
+    fx(123);
 }
